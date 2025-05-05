b3462de1079896e724c34ddd0e8f3b636114b1c1
Alexander Stekolshchikov <alexstekk@yandex.ru>
2025-05-05 13:01:26 +0300

    add rtk query, inject endpoints, refactor for pages

--- src/features/editableProfileCard/ui/EditableProfileCard/EditableProfileCard.tsx
+++ src/features/editableProfileCard/ui/EditableProfileCard/EditableProfileCard.tsx
@@ -3,29 +3,22 @@ import { useTranslation } from 'react-i18next';
 import cls from './EditableProfileCard.module.scss';
 import { memo, useCallback } from 'react';
 import { useAppDispatch, useAppSelector } from 'shared/lib/hooks/redux/reduxTypedHooks';
-import { useParams } from 'react-router-dom';
 import { Currency } from 'entities/Currency';
 import { Country } from 'entities/Country';
 import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
 import { Text, TextVariants } from 'shared/ui/Text/Text';
 import { getProfileForm } from '../../model/selectors/getProfileForm/getProfileForm';
 import { getProfileError } from '../../model/selectors/getProfileError/getProfileError';
-import {
-    getProfileIsLoading
-} from '../../model/selectors/getProfileIsLoading/getProfileIsLoading';
+import { getProfileIsLoading } from '../../model/selectors/getProfileIsLoading/getProfileIsLoading';
 import { getProfileReadonly } from '../../model/selectors/getProfileReadonly/getProfileReadonly';
-import {
-    getProfileValidateErrors
-} from '../../model/selectors/getProfileValidateErrors/getProfileValidateErrors';
+import { getProfileValidateErrors } from '../../model/selectors/getProfileValidateErrors/getProfileValidateErrors';
 import { profileActions, profileReducer } from '../../model/slice/profileSlice';
 import { fetchProfileData } from '../../model/services/fetchProfileData/fetchProfileData';
 import { ProfileCard } from 'entities/Profile';
 import { ValidateProfileError } from '../../model/types/editableProfileCardSchema';
 import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 import { VStack } from 'shared/ui/Stack';
-import {
-    EditableProfileCardHeader
-} from 'features/editableProfileCard/ui/EditableProfileCardHeader/EditableProfileCardHeader';
+import { EditableProfileCardHeader } from '../EditableProfileCardHeader/EditableProfileCardHeader';
 
 interface EditableProfileCardProps {
     className?: string;
