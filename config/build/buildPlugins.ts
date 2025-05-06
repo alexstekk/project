78a80656c7f84a8cf35a8adc1831c12a76c65184
Alexander Stekolshchikov <alexstekk@yandex.ru>
2025-05-06 10:59:55 +0300

    add Circular Dependency Plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -5,6 +5,7 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import ReactRefreshPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer';
 import CopyPlugin from "copy-webpack-plugin";
+import CircularDependencyPlugin from 'circular-dependency-plugin';
 
 export function buildPlugins({paths, isDev, apiUrl, project}: BuildOptions): webpack.WebpackPluginInstance[] {
 
@@ -37,7 +38,21 @@ export function buildPlugins({paths, isDev, apiUrl, project}: BuildOptions): web
             ),
             new BundleAnalyzerPlugin({
                 openAnalyzer: false,
-            }));
+            }),
+            new CircularDependencyPlugin({
+                // exclude detection of files based on a RegExp
+                exclude: /a\.js|node_modules/,
+                // include specific files based on a RegExp
+                // include: /dir/,
+                // add errors to webpack instead of warnings
+                failOnError: true,
+                // allow import cycles that include an asyncronous import,
+                // e.g. via import(/* webpackMode: "weak" */ './file.js')
+                allowAsyncCycles: false,
+                // set the current working directory for displaying module paths
+                cwd: process.cwd(),
+            })
+        )
     }
 
 
