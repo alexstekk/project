25815aadd1233232e6375d744adf1044c923fa49
Alexander Stekolshchikov <alexstekk@yandex.ru>
2025-05-17 11:36:13 +0300

    ci reports

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -129,15 +129,15 @@ const config: Config = {
     // Use this configuration option to add custom reporters to Jest
     // reporters: undefined,
 
-    // "reporters": [
-    //     "default",
-    //     ["jest-html-reporters", {
-    //         "publicPath": "<rootDir>/reports/unit",
-    //         "filename": "report.html",
-    //         "openReport": true,
-    //         inlineSource: true,
-    //     }]
-    // ],
+    "reporters": [
+        "default",
+        ["jest-html-reporters", {
+            "publicPath": "<rootDir>/reports/unit",
+            "filename": "report.html",
+            // "openReport": true,
+            inlineSource: true,
+        }]
+    ],
 
     // Automatically reset mock state before every test
     // resetMocks: false,
